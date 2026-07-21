a0b99591b1fea6143c9cf4b5dedb952282cd3fc3
urstark <urstark@users.noreply.github.com>
2026-07-21 11:59:03 +0530

    initial server setup

---
+++ README.md
@@ -0,0 +1,8 @@
+python script middelware in flask
+
+Guide:
+
+`python3 -m venv .venv`
+`source .venv/bin/activate`
+
+`pip install -r requirements.txt`
--- server.py
+++ server.py
@@ -6,11 +6,26 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 SHARED_SECRET = B"my_private_signing_key_2026"
-REQUEST_VALIDITY_WINDOW = 30 #signature validity is 30s
+VALIDITY_WINDOW = 30 #signature validity is 30s
 
 def verify_request_intergity():
     timestamp = request.headers.get("X-Timestamp")
     signature = request.headers.get("x-Signature")
 
     if not timestamp or not signature:
-        return Flase, "Missing NTrust Headers (X-Timestamp / X-Signature)"
+        return Flase, "Missing NTrust Security Headers (X-Timestamp / X-Signature)"
+
+    try:
+        if abs(int(time.time()) - int(timestamp)) > VALIDITY_WINDOW:
+            return Flase, "Your request is expired or replay attact detected."
+        except ValueError:
+            return False, "Invalid timestamp formate"
+
+    body = request.get_data(as_text=True)
+    message = f"{timestamp}:{request.method}:{request.path}:{body}".encode("utf-8")
+    expected_sig = hmac.new)SHARED_SECRET
+
+    
+
+
+    
