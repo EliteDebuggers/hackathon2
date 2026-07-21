0eb0699f970b58bf9482a61b99620aa95704aeff
urstark <urstark@users.noreply.github.com>
2026-07-21 11:44:34 +0530

    NTrust project start

--- server.py
+++ server.py
@@ -4,3 +4,13 @@ import time
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+SHARED_SECRET = B"my_private_signing_key_2026"
+REQUEST_VALIDITY_WINDOW = 30 #signature validity is 30s
+
+def verify_request_intergity():
+    timestamp = request.headers.get("X-Timestamp")
+    signature = request.headers.get("x-Signature")
+
+    if not timestamp or not signature:
+        return Flase, "Missing NTrust Headers (X-Timestamp / X-Signature)"
