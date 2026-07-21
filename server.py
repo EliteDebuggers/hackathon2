2dd00d9d555e1c9afe94988da8ec24b4223464e0
urstark <urstark@users.noreply.github.com>
2026-07-21 12:55:57 +0530

    fixed typos in server.py

--- server.py
+++ server.py
@@ -10,22 +10,22 @@ VALIDITY_WINDOW = 30 #signature validity is 30s
 
 # server setup hmac
 
-def verify_request_intergity():
+def verify_request_integrity():
     timestamp = request.headers.get("X-Timestamp")
     signature = request.headers.get("x-Signature")
 
     if not timestamp or not signature:
-        return Flase, "Missing NTrust Security Headers"
+        return False, "Missing NTrust Security Headers"
 
     try:
         if abs(int(time.time()) - int(timestamp)) > VALIDITY_WINDOW:
-            return Flase, "Your request is expired or replay attact detected!"
-        except ValueError:
+            return False, "Your request is expired or replay attact detected!"
+    except ValueError:
             return False, "Invalid timestamp format"
 
     body = request.get_data(as_text=True)
     message = f"{timestamp}:{request.method}:{request.path}:{body}".encode("utf-8")
-    expected_sig = hmac.new(SHARED_SECRET, message, hasglib.sha256),hexdigest()
+    expected_sig = hmac.new(SHARED_SECRET, message, hashlib.sha256),hexdigest()
 
     if not hmac.compare_digest(expected_sig, signature):
         return False, "Invalid Signature! Potential tampering or unauthorised source code exploit."
@@ -37,7 +37,7 @@ def enforce_ntrust_security():
     if request.path == "/health":
         return
 
-    is-valid, reason = verify_request_intergity()
+    is_valid, reason = verify_request_integrity()
     if not is_valid:
         return jsonify({
             "status": "BLOCKED BY NTRUST",
